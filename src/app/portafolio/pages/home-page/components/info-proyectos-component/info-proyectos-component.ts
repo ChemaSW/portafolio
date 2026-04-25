006239194b795aa9dcc698a558374bddf63f35e5
ChemaSW <jm.contreras.roch@gmail.com>
2026-04-24 22:31:18 -0600

    routerLink de mas proyectos en Home

--- src/app/portafolio/pages/home-page/components/info-proyectos-component/info-proyectos-component.ts
+++ src/app/portafolio/pages/home-page/components/info-proyectos-component/info-proyectos-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'info-proyectos-component',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './info-proyectos-component.html',
 })
 export class InfoProyectosComponent { }
